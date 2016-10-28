17fc1194a652d53c2c648ecaabb9554c174fe5ac
VikiPeeva <peeva_viki@yahoo.com>
2016-10-28 17:31:28 +0200

    initial commit

---
+++ src/client/app/pages/auth-pages/auth-pages.module.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Viki on 10/26/2016.
+ */
+
+import {NgModule} from "@angular/core";
+import {SharedModule} from "../../shared/shared.module";
+
+@NgModule({
+  imports: [SharedModule.forRoot()]
+})
