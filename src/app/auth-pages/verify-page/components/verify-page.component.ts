3df672b44d4e47ba2c98e788e8d90e398ffad54c
VikiPeeva <peeva_viki@yahoo.com>
2017-05-15 23:02:59 +0200

    #659985/Inform the user to check the email for activation code - resend button added

--- src/app/auth-pages/verify-page/components/verify-page.component.ts
+++ src/app/auth-pages/verify-page/components/verify-page.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {UserObjectService} from "../../../domain/services/user/user-object.service";
 import {User} from "../../../domain/model/authentication/user";
+import {UserService} from "../../../domain/services/user/user.service";
+import {MdSnackBar} from "@angular/material";
 
 @Component({
     selector: 'ideal-verify-page',
@@ -12,12 +14,19 @@ export class VerifyPageComponent implements OnInit {
 
     private user: User;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private userService: UserService,
+                private snackBar: MdSnackBar) {
     }
 
     ngOnInit() {
         this.route.data.subscribe((data: {user: User}) => {
             this.user = data.user;
+        });
+    }
+
+    resendActivationCode(): void {
+        this.userService.resendActivationCode(this.user.email).subscribe(() => {
+            this.snackBar.open("Your activation code was resent", undefined, {duration: 3000});
         });
     }
 
