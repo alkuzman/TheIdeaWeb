1fe36739fc4d9d1868a504a7b7bc3a85e8d673ac
aleks <aleksandarkzuamnoski11@gmail.com>
2018-07-10 23:17:32 +0200

    DL-45 : Upgrate to Chips with Input
    - keyword selector rewritten

--- src/app/domain/keyword/keyword.module.ts
+++ src/app/domain/keyword/keyword.module.ts
@@ -1,14 +1,14 @@
 /**
  * Created by AKuzmanoski on 19/02/2017.
  */
-import {NgModule} from "@angular/core";
-import {SharedModule} from "../../shared/shared.module";
-import {KeywordSearchComponent} from "./components/keyword-search/keyword-search.component";
-import {KeywordSelectorComponent} from "./components/keyword-selector/keyword-selector.component";
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../../shared/shared.module';
+import {KeywordSelectorComponent} from './components/keyword-selector/keyword-selector.component';
+
 @NgModule({
   imports: [SharedModule],
-  declarations: [KeywordSearchComponent, KeywordSelectorComponent],
-  exports: [KeywordSearchComponent, KeywordSelectorComponent]
+  declarations: [KeywordSelectorComponent],
+  exports: [KeywordSelectorComponent]
 })
 export class KeywordModule {
 
