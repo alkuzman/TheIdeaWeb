82be4b6f0c3629dfa8059df417a396dc43bf7743
VikiPeeva <peeva_viki@yahoo.com>
2018-07-25 20:37:23 +0200

    Remove key from local store when object is null

--- src/app/core/store/local-store.ts
+++ src/app/core/store/local-store.ts
@@ -2,33 +2,38 @@ import {PersistentStore} from './persistent-store';
 import {Observable, of} from 'rxjs';
 
 export class LocalStore implements PersistentStore {
-  get<T>(key: string): Observable<T> {
-    const data = localStorage.getItem(key);
-    let object;
-    try {
-      object = JSON.parse(data);
-    } catch (e) {
-      if (data === 'undefined') {
-        object = undefined;
-      } else {
-        object = data;
-      }
-    }
-    return of(object);
-  }
-
-  store<T>(key: string, object: T): void {
-    let data;
-    try {
-      data = JSON.stringify(object);
-    } catch (e) {
-      data = object;
+    get<T>(key: string): Observable<T> {
+        const data = localStorage.getItem(key);
+        let object;
+        try {
+            object = JSON.parse(data);
+        } catch (e) {
+            if (data == null || data === 'undefined') {
+                object = undefined;
+            } else {
+                object = data;
+            }
+        }
+        return of(object);
     }
 
-    localStorage.setItem(key, data);
-  }
+    store<T>(key: string, object: T): void {
+        if (object == null) {
+            localStorage.removeItem(key);
+            return;
+        }
 
-  remove(key: string): void {
-    localStorage.removeItem(key);
-  }
+        let data;
+        try {
+            data = JSON.stringify(object);
+        } catch (e) {
+            data = object;
+        }
+
+        localStorage.setItem(key, data);
+    }
+
+    remove(key: string): void {
+        localStorage.removeItem(key);
+    }
 }
